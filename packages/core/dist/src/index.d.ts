11db6060be88c27cb843dd208ff53ccee88a37d5
Nick Schrock <nick.a.schrock@gmail.com>
2025-08-11 23:05:40 -0400

    clean

--- packages/core/dist/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { MultiAgentFeatureOrchestrator as Orchestrator } from './orchestrator';
-export { InteractiveMultiAgentCLI as InteractiveUI } from './interactive';
-export * from './utils';
-//# sourceMappingURL=index.d.ts.map
