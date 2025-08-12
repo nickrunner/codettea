e11a4eb94edc50714eee1a15902bbaa60a7df900
Nick Schrock <nick.a.schrock@gmail.com>
2025-08-11 20:35:03 -0400

    fix: removed build files and added clean

--- packages/core/dist/src/run-feature.d.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env tsx
-declare function main(): Promise<void>;
-export { main };
-//# sourceMappingURL=run-feature.d.ts.map
--- packages/core/dist/src/test-runner.d.ts
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env tsx
-export {};
-//# sourceMappingURL=test-runner.d.ts.map
